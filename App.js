57a48335d6e0d7aa329d813d559ab918a969e557
ezeanic <ezeanic@uindy.edu>
2020-12-15 04:11:20 -0500

    App can add to do items

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import InputBar from './components/InputBar';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+   constructor(){
+    super();
+    this.state={
+      todoInput: '',
+      todos: [{id: 0, title: 'Take out trash', done: false},
+      {id: 1, title: 'Take out trash', done: false}]
+    }
+  }
+
+  addNewTodo(){
+    let todos = this.state.todos;
+    todos.push(
+      {id: todos.length, title: this.state.todoInput, done: false}
+    );
+    
+    this.state.todos = todos;
+    console.log(this.state.todos);
+    //make input nothing after adding new
+    this.setState({
+      todoInput: '',
+    })
+  }
+  
+  render(){
+    let items = []
+    this.state.todos.forEach(element => {
+      items.push(<Text>{element.title}</Text>)
+    });
+
+    return (
+      <View style={styles.container}>
+        <Header title="Todoapp"/>
+        <InputBar textChange={todoInput => this.setState({ todoInput})} addNewTodo={()=>{this.addNewTodo()}}/>
+        {items}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
